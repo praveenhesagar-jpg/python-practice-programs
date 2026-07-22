1f94ab9146e96aa6481647c57c2cf34cb98b7b00
Praveen <praveenhesagar@gmail.comhe>
2026-07-22 20:23:39 +0530

    add count odd number

---
+++ count_odd.py
@@ -0,0 +1,12 @@
+def find_odd_numbers(arr):
+    count =0
+    for num in arr:
+        if num % 2 !=0:
+            count += 1
+    return count
+        
+
+# Call the function
+numbers = [5,4,8,3,11,17,21]
+odd_count = find_odd_numbers(numbers)
+print(f"The count of odd numbers in the list is: {odd_count}")
